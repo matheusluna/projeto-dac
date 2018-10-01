84fb9dd9e8cd473238c497bc90bedf70cc498c9e
Lucas Duete <lm.duete@gmail.com>
2018-09-30 23:45:17 -0300

    Registrando filtros no projeto.

--- rhecruta-java/src/main/java/io/github/dac/rhecruta/rest/Loader.java
+++ rhecruta-java/src/main/java/io/github/dac/rhecruta/rest/Loader.java
@@ -1,5 +1,8 @@
 package io.github.dac.rhecruta.rest;
 
+import io.github.dac.rhecruta.rest.infraSecurity.filters.CORSFilter;
+import io.github.dac.rhecruta.rest.infraSecurity.filters.FilterSecurityAuthentication;
+import io.github.dac.rhecruta.rest.infraSecurity.filters.FilterSecurityAuthorization;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
@@ -9,5 +12,9 @@ public class Loader extends ResourceConfig {
 
     public Loader() {
         packages("io.github.dac.rhecruta.rest.controllers");
+
+        register(CORSFilter.class);
+        register(FilterSecurityAuthorization.class);
+        register(FilterSecurityAuthentication.class);
     }
 }
