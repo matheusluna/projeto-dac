bdd31688dc1e95b02b746c39e5a2d8c92afe6612
Lucas Duete <lm.duete@gmail.com>
2018-09-30 23:29:03 -0300

    Implementando Service para Entrevista.

---
+++ rhecruta-java/src/main/java/io/github/dac/rhecruta/service/EntrevistaService.java
@@ -0,0 +1,34 @@
+package io.github.dac.rhecruta.service;
+
+import io.github.dac.rhecruta.dao.interfaces.EntrevistaDaoInterface;
+import io.github.dac.rhecruta.models.Candidatura;
+import io.github.dac.rhecruta.models.Entrevista;
+
+import javax.ejb.EJB;
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import java.util.List;
+
+@Local
+@Stateless
+public class EntrevistaService {
+
+    @EJB
+    private EntrevistaDaoInterface entrevistaDao;
+
+    public void salvar(Entrevista entrevista) {
+        entrevistaDao.salvar(entrevista);
+    }
+
+    public void remover(Entrevista entrevista) {
+        entrevistaDao.remover(entrevista);
+    }
+
+    public void atualizar(Entrevista entrevista) {
+        entrevistaDao.atualizar(entrevista);
+    }
+
+    public List<Entrevista> entrevistasPorCandidatura(Candidatura candidatura) {
+        return entrevistaDao.entrevistasPorCandidatura(candidatura);
+    }
+}
