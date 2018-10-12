9ea4da033237c098679ce242eaf43446714845d9
Lucas Duete <lm.duete@gmail.com>
2018-10-12 15:01:28 -0300

    Implementando método que adiciona interesse, remove interesse a lista todos os interesses de um Candidato em CandidatoController.

--- rhecruta-java/src/main/java/io/github/dac/rhecruta/rest/controllers/CandidatoController.java
+++ rhecruta-java/src/main/java/io/github/dac/rhecruta/rest/controllers/CandidatoController.java
@@ -1,15 +1,15 @@
 package io.github.dac.rhecruta.rest.controllers;
 
 import io.github.dac.rhecruta.models.Candidato;
+import io.github.dac.rhecruta.rest.infraSecurity.Security;
+import io.github.dac.rhecruta.rest.infraSecurity.TokenManagement;
 import io.github.dac.rhecruta.service.CandidatoService;
 
 import javax.ejb.EJB;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.*;
 import java.net.URI;
+import java.util.List;
 
 @Path("candidato")
 public class CandidatoController {
@@ -30,7 +30,7 @@ public class CandidatoController {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response cadastrarCandidado(Candidato candidato, @Context UriInfo uriInfo) {
 
-        String cpf = candidatoService.salvar(candidato);
+        String cpf = this.candidatoService.salvar(candidato);
         URI uri = uriInfo.getAbsolutePathBuilder().path(cpf).build();
 
         return Response.created(uri).build();
@@ -60,7 +60,7 @@ public class CandidatoController {
         if (cpf == null || cpf.isEmpty() || cpf.length() != 11)
             return Response.status(Response.Status.BAD_REQUEST).build();
 
-        Candidato candidato = candidatoService.candidatoComCPF(cpf);
+        Candidato candidato = this.candidatoService.candidatoComCPF(cpf);
 
         if (candidato == null)
             return Response.status(Response.Status.NO_CONTENT).build();
@@ -76,12 +76,81 @@ public class CandidatoController {
         if (email == null || email.length() < 3 || !email.contains("@"))
             return Response.status(Response.Status.BAD_REQUEST).build();
 
-        Candidato candidato = candidatoService.candidatoComEmail(email);
+        Candidato candidato = this.candidatoService.candidatoComEmail(email);
 
         if (candidato == null)
             return Response.status(Response.Status.NO_CONTENT).build();
         else
             return Response.ok(candidato).build();
+    }
+
+    @GET
+    @Path("interesse")
+    @Security
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response recuperarInteresses(@Context SecurityContext securityContext) {
+
+        String token = TokenManagement.getToken(securityContext);
+
+        Candidato candidato = this.candidatoService.candidatoComEmail(token);
+
+        List<Integer> interesses = candidato.getInteresses();
+
+        if (interesses != null && interesses.size() > 0)
+            return Response.ok(interesses).build();
+        else
+            return Response.noContent().build();
+
+    }
+
+    @POST
+    @Path("interesse")
+    @Security
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response adicionarInteresse(@FormParam("idVaga") Integer idVaga,
+                                       @Context SecurityContext securityContext) {
+
+        if (idVaga == null || idVaga <= 0)
+            return Response.status(Response.Status.BAD_REQUEST).build();
+
+        String token = TokenManagement.getToken(securityContext);
+
+        Candidato candidato = this.candidatoService.candidatoComEmail(token);
+
+        if (candidato.getInteresses().contains(idVaga))
+            return Response.status(Response.Status.NOT_MODIFIED).build();
+
+
+        candidato.adicionarInteresse(idVaga);
+
+        candidatoService.atualizar(candidato);
+
+        return Response.ok().build();
+
+    }
+
+    @DELETE
+    @Path("interesse/{idVaga}")
+    @Security
+    public Response removerInteresse(@PathParam("idVaga") Integer idVaga,
+                                     @Context SecurityContext securityContext) {
+
+        if (idVaga == null || idVaga <= 0)
+            return Response.status(Response.Status.BAD_REQUEST).build();
+
+        String token = TokenManagement.getToken(securityContext);
+
+        Candidato candidato = this.candidatoService.candidatoComEmail(token);
+
+        if (!candidato.getInteresses().contains(idVaga))
+            return Response.status(Response.Status.NOT_MODIFIED).build();
+
+        candidato.removerInteresse(idVaga);
+
+        this.candidatoService.atualizar(candidato);
+
+        return Response.ok().build();
+
     }
 
 }
