cae010a11f517df6ee224e622ad1771218cc7342
Lucas Duete <lm.duete@gmail.com>
2018-10-14 20:40:00 -0300

    Tornando CandidaturaController de Session Scope para persistir estado entre requisições.

--- rhecruta-java/src/main/java/io/github/dac/rhecruta/web/controllers/CandidaturaController.java
+++ rhecruta-java/src/main/java/io/github/dac/rhecruta/web/controllers/CandidaturaController.java
@@ -9,17 +9,19 @@ import io.github.dac.rhecruta.service.VagaService;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.json.JsonObject;
 import javax.persistence.criteria.CriteriaBuilder;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.List;
 
 @Named
-@RequestScoped
-public class CandidaturaController {
+@SessionScoped
+public class CandidaturaController implements Serializable {
 
     @EJB
     private CandidaturaService candidaturaService;
