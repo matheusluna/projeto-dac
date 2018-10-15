206ef9fdd8d34c9ca4c914185e6bcb8a2d28ab59
Matheus Moreira Luna <matheusluna96@gmail.com>
2018-10-15 04:52:20 -0300

    adicionando docker

---
+++ rhecruta-java/kill.sh
@@ -0,0 +1,7 @@
+mvn clean
+docker stop app
+docker rm app
+docker rmi dac/app
+docker stop banco
+docker rm banco
+docker rmi dac/banco
---
+++ rhecruta-java/run.sh
@@ -0,0 +1,5 @@
+mvn clean package
+docker build -t dac/banco ./banco
+docker build -t dac/app .
+docker run -p 5433:5432 --name banco -d dac/banco
+docker run -p 8081:8080 --name app --link banco:host-banco dac/app
